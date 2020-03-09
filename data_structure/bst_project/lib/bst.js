38078819e11d52ddf00b1f27ecde460d800742cc
Peizhi Zheng <parisa1206@gmail.com>
2020-03-09 15:36:25 -0700

    bst

---
+++ data_structure/bst_project/lib/bst.js
@@ -0,0 +1,63 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BST {
+   constructor(){
+       this.root = null
+   }
+   insert(value, root = this.root){
+       if (!root){
+        this.root = new TreeNode(value)
+        return
+        }
+        if (value < root.val){
+            if(!root.left){
+                root.left = new TreeNode(value)
+            }else{
+                this.insert(value,root.left)
+            }
+        } else {
+            if(!root.right){
+                root.right = new TreeNode(value)
+            }else{
+                this.insert(value,root.right)
+            }
+        } 
+   }
+   searchRecur(val, root=this.root) {
+    if (!root) return false;
+
+    if (val<root.val){
+        return this.searchRecur(val,root.left)
+    } else if (val > root.val){
+        return this.searchRecur(val,root.right)
+    }else{
+        return true
+        }
+   }
+   searchIter(val){
+        let curr=this.root
+        while(curr){
+           if (val<curr.val){
+            curr = curr.left 
+           } else if (val > curr.val){
+               curr = curr.right
+           } else{
+               return true
+           }
+        }
+    return false
+   }
+}
+
+
+module.exports = {
+    TreeNode,
+    BST
+};
