0d77ded63d0ce808aaeba4e91fe91302de3a7f83
Peizhi Zheng <parisa1206@gmail.com>
2020-04-13 17:23:49 -0700

    ttt game

---
+++ tictactoe/board.rb
@@ -0,0 +1,57 @@
+class Board
+
+    def initialize
+        @grid = Array.new(3) { Array.new(3, '_') }
+    end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @grid[row][col] = val
+    end
+
+    def valid?(pos)
+        row, col = pos
+        pos.all? do |i|
+            0 <= i && i < @grid.length
+        end
+    end
+
+    def empty?(pos)
+        self[pos] == '_'
+    end
+
+    def place_mark(pos, mark)
+        raise 'invalid mark' if !valid?(pos) || !empty?(pos)
+        self[pos] = mark
+    end
+
+    def print
+        @grid.each do |row|
+            p row
+        end
+    end
+
+    def win_row?(mark)
+        @grid.any? { |row| row.all?(mark)}
+    end
+
+    def win_col?(mark)
+        @grid.any? { |col| col.all?(mark)}
+    end
+
+    def win_diagonal?(mark)
+
+    end
+    def win?(mark)
+        win_col?(mark) || win_row?(mark) || win_diagonal?(mark)
+    end
+
+    def empty_positions?
+        @grid.any? {}
+    end
+end
