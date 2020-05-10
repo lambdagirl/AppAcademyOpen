d7b4de2ba9ca40116b1f51c8330ce0825035486b
Peizhi Zheng <parisa1206@gmail.com>
2020-05-10 00:21:02 -0700

    fix bug

--- W6D3/Plays-Playwrights-ORM/plays.rb
+++ W6D3/Plays-Playwrights-ORM/plays.rb
@@ -1,15 +1,6 @@
 require 'sqlite3'
 require 'singleton'
 
-class PlayDBConnection < SQLite3::Database
-  include Singleton
-
-  def initialize
-    super('plays.db')
-    self.type_translation = true
-    self.results_as_hash = true
-  end
-end
 
 class Play
   attr_accessor :id, :title, :year, :playwright_id
