e0d2a8b6c20cf389ecb36eb87f45e250b8d4c61f
Peizhi Zheng <parisa1206@gmail.com>
2020-05-10 23:30:59 -0700

    add aa bonus

--- W6D3/AA Questions/model.rb
+++ W6D3/AA Questions/model.rb
@@ -15,5 +15,51 @@ class ModelBase
     data.nil? ? nil : self.new(data)
   end
 
+  def self.all
+    data = QuestionsDatabase.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table}
+    SQL
 
+    data.map { |attr| self.new(attrs) }
+  end
+
+  def self.where(params)
+    if params.is_a?(Hash)
+      where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+      vals = params.values
+    else
+      where_line = params
+      vals = []
+    end
+
+    data = QuestionsDatabase.execute(<<-SQL, *vals)
+      SELECT
+        *
+      FROM
+        #{self.table}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(data)
+  end
+
+  def self.find_by(params)
+    self.where(params)
+  end
+
+  def self.first
+    data = QuestionsDatabase.get_first_row(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table}
+      LIMIT
+        1
+    SQL
+
+    self.new(data)
 end
