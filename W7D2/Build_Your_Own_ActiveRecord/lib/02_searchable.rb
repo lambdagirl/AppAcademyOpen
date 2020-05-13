2e97467d4abc4d37b40c3edee34091bee27dab2a
Peizhi Zheng <parisa1206@gmail.com>
2020-05-13 09:32:25 -0700

    active-record finished phase 2

--- W7D2/Build_Your_Own_ActiveRecord/lib/02_searchable.rb
+++ W7D2/Build_Your_Own_ActiveRecord/lib/02_searchable.rb
@@ -3,10 +3,18 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    params = params.map {|col,val| "#{col}='#{val}'"}.join(" AND\n")
+    results = DBConnection.execute(<<-SQL)
+      SELECT *
+      FROM #{table_name}
+      WHERE #{params}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
