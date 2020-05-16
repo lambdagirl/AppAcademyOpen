c065aaf689956235249653d603d058287386f9aa
Peizhi Zheng <parisa1206@gmail.com>
2020-05-16 15:53:08 -0700

    rendered views for 99 cats

---
+++ W7D5/cats99/app/controllers/cats_controller.rb
@@ -0,0 +1,16 @@
+class CatsController < ApplicationController
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+end
