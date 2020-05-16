1465a4ab0013450fd393d533343d051799014fa8
Peizhi Zheng <parisa1206@gmail.com>
2020-05-16 15:27:38 -0700

    Phase 1: add model

---
+++ W7D5/cats99/app/models/cat.rb
@@ -0,0 +1,14 @@
+require 'date'
+class Cat < ApplicationRecord
+  validates :birth_date, :color, :name, :sex, presence: true
+  validates :color, inclusion: {in: ['red', 'yellow', 'white', 'black', 'brown', 'multi-color'],
+          message: "%{value} is not a valid color"}
+  validates :sex, inclusion: {in: ['F', 'M'],
+          message: "%{value} is not a valid sex"}  
+  
+  def age
+    diff = DateTime.now() - birth_date
+    age = (diff/365).to_i
+    age
+  end
+end
