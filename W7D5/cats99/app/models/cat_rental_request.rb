7dea9ece8f3b8c54a65b9b9bd5d7c6f57a0561b5
Peizhi Zheng <parisa1206@gmail.com>
2020-05-16 22:14:04 -0700

    add custom validation

--- W7D5/cats99/app/models/cat_rental_request.rb
+++ W7D5/cats99/app/models/cat_rental_request.rb
@@ -1,7 +1,27 @@
 class CatRentalRequest < ApplicationRecord
   validates :cat_id, :end_date, :start_date, :status, presence: true
-  validates :status, inclusion: {in: ['PENDDING', 'APPROVED', 'DENIED']}
+  validates :status, inclusion: {in: ['PENDING', 'APPROVED', 'DENIED']}
 
   belongs_to :cat
 
+  def overlapping_requests
+    CatRentalRequest
+      .where.not(id: self.id)
+      .where(cat_id: self.id)
+      .where("start_date < ? AND end_date > ?", self.end_date, self.start_date)
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where(status: "APPROVED")
+  end
+  def overlapping_pending_requests
+    overlapping_requests.where(status: "PENDING")
+  end
+
+  def does_not_overlap_approved_request
+    # We cannot rent out the same cat to multiple people at the same time
+    if verlapping_approved_requests.exist?
+      errors[:base] << 'Request conflicts with existing approved request'
+    end
+  end
 end
