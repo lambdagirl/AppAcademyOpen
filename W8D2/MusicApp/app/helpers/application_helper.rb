b6e47310802b5c298d1fcff9f975476d7c08975b
Peizhi Zheng <pzheng16@asu.edu>
2020-05-21 21:26:28 -0700

    add helper view

--- W8D2/MusicApp/app/helpers/application_helper.rb
+++ W8D2/MusicApp/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def ugly_notes(note)
+    formatted_note=""
+    note.lines.each do |line|
+      formatted_note << "✔️ #{h(line)}"
+    end
+
+    "<pre class='note'>#{formatted_note}</pre>".html_safe
+  end
 end
