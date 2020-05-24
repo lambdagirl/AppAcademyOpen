08f32f614f06e92705683a6746d4e396a0096ae1
Peizhi Zheng <pzheng16@asu.edu>
2020-05-24 00:17:52 -0700

    add gear drop down

--- W8D4/AATimes/app/assets/javascripts/components/dropdown.js
+++ W8D4/AATimes/app/assets/javascripts/components/dropdown.js
@@ -3,15 +3,15 @@
 // dropdown function that removes "hidden" class from dropdown while
 // adding hideDropdown listener to document and cleaning up out-of-date listener
 const revealDropdown = (event) => {
-  event.stopPropagation(); // prevent event from being picked up by body
-	$('#gear-dropdown').removeClass('hidden');
+  event.stopPropagation();  // prevent event from being picked up by body
+  $('#gear-dropdown').removeClass('hidden');
   $('#gear-dropdown-btn').off('click', revealDropdown);
   $(document).on('click', hideDropdown);
 };
 
 // add "hidden" class to dropdown and update event listeners
 const hideDropdown = () => {
-	$('#gear-dropdown').addClass('hidden');
+  $('#gear-dropdown').addClass('hidden');
   $('#gear-dropdown-btn').on('click', revealDropdown);
   $(document).off('click', hideDropdown);
 };
