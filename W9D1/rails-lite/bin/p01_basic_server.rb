1b56792c60f448aed88dcb170a6ac6fbedc1d395
Peizhi Zheng <pzheng16@asu.edu>
2020-05-27 00:11:02 -0700

    finished railslite phase1

--- W9D1/rails-lite/bin/p01_basic_server.rb
+++ W9D1/rails-lite/bin/p01_basic_server.rb
@@ -1,15 +1,35 @@
 require 'rack'
 
-class App
-  def call(env)
-    http_status ='200'
-    headers ={'Content-Type' => 'text/html'}
-    body = ['hello word']
+# class App
+#   def call(env)
+#     http_status ='200'
+#     headers ={'Content-Type' => 'text/html'}
+#     body = ['hello word']
 
-    [http_status, headers, body]
-  end
+#     [http_status, headers, body]
+#   end
+# end
+
+# Rack::Server.start(
+#   app:App.new
+# )
+
+######basic Rack########
+# Rack::Server.start(
+#   app: Proc.new do |env|
+#     ['200', {'Content-Type' => 'text/html'},['hello world']]
+#   end
+# )
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write(req.fullpath)
+  res.finish
 end
 
 Rack::Server.start(
-  app:App.new
+  app:app,
+  Port: 3000
 )
