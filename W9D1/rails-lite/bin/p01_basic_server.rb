a5b0913a4311703c0e5fef9a19cef9dbdc27b9d0
Peizhi Zheng <pzheng16@asu.edu>
2020-05-27 00:00:36 -0700

    phase1 basic server

--- W9D1/rails-lite/bin/p01_basic_server.rb
+++ W9D1/rails-lite/bin/p01_basic_server.rb
@@ -1,1 +1,15 @@
 require 'rack'
+
+class App
+  def call(env)
+    http_status ='200'
+    headers ={'Content-Type' => 'text/html'}
+    body = ['hello word']
+
+    [http_status, headers, body]
+  end
+end
+
+Rack::Server.start(
+  app:App.new
+)
