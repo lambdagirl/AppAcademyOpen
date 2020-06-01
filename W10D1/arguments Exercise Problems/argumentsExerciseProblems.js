49c078f6783aecd7342c3bff6a9aee08e8243075
Peizhi Zheng <pzheng16@asu.edu>
2020-06-01 10:36:44 -0700

    sumCurry

--- W10D1/arguments Exercise Problems/argumentsExerciseProblems.js
+++ W10D1/arguments Exercise Problems/argumentsExerciseProblems.js
@@ -13,3 +13,36 @@ function sum1(...nums) {
   });
   return total;
 }
+
+Function.prototype.myBind1 = function(ctx){
+    const fn = this;
+    const bindArgs = Array.from(arguments).slice(1);
+    return function _boundFn(){
+        const callArgs = Array.from(arguments);
+        return fn.apply(ctx,bindArgs.concat(callArgs));
+    };
+};
+
+Function.prototype.myBind2 = function(ctx, ...bindArgs){
+    return (...callArgs) => this.apply(ctx, bindArgs.concat(callArgs));
+};
+
+function curriedSum(numArgs){
+    const nums=[];
+
+    function _curriedSum(num) {
+        nums.push(num);
+        if (nums.length === numArgs){
+            let total = 0;
+            nums.forEach((n)=>{total += n;});
+            return total;
+        } else{
+            return _curriedSum;
+        }
+    }
+    return _curriedSum;
+}
+const sum4 = curriedSum(4);
+sum4(5)(30)(20)(1); // => 56
+//_curriedSum keeps collecting arguments and returning itself until it has enough arguments, at which point it actually does the required work of summing.
+
