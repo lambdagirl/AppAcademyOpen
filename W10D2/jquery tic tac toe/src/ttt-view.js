055df43b31dbe84fb73897bed918b0fbf7ed4a98
Peizhi Zheng <pzheng16@asu.edu>
2020-06-02 23:29:59 -0700

    finish place move

--- W10D2/jquery tic tac toe/src/ttt-view.js
+++ W10D2/jquery tic tac toe/src/ttt-view.js
@@ -2,24 +2,43 @@ class View {
   constructor(game, $el) {
     this.game = game;
     this.$el = $el;
-    this.setupBoard()
+    this.setupBoard();
+    this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$el.on("click", "li", (event) => {
+      const $square = $(event.currentTarget);
+      this.makeMove($square);
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    const pos = $square.data("pos");
+    const currentPlayer = this.game.currentPlayer;
+
+    try {
+      this.game.playMove(pos);
+    } catch (e) {
+      alert("This " + e.msg.toLowerCase());
+      return;
+    }
+
+    $square.addClass(currentPlayer);
+
+  }
 
   setupBoard() {
     const $ul = $("<ul></ul>");
 
-    for (let r = 0; r < 3; r++){
-      for (let c = 0; c < 3; c++){
+    for (let r = 0; r < 3; r++) {
+      for (let c = 0; c < 3; c++) {
         let $li = $("<li></li");
-        $li.data("pos", [r,c]);
+        $li.data("pos", [r, c]);
         $ul.append($li);
       }
     }
-    this.$el.append($ul)
+    this.$el.append($ul);
   }
 }
 
