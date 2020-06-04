b2735f3373aea6c2e88bd1d31e1b7df5dc28a74f
Peizhi Zheng <pzheng16@asu.edu>
2020-06-04 07:32:12 -0700

    finished game_view

--- W10D3/asteroids/src/game_view.js
+++ W10D3/asteroids/src/game_view.js
@@ -2,3 +2,15 @@
 // Stores a canvas context to draw the game into.
 // Installs key listeners to move the ship and fire bullets.
 // Installs a timer to call Game.prototype.step.
+
+function GameView(game,ctx){
+    this.ctx = ctx;
+    this.game = game;
+}
+GameView.MOVES = {
+    w:[0,-1],
+    a:[-1,0],
+    s:[0,1],
+    d:[1,0],
+}
+
